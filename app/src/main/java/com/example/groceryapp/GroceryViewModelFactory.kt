71f2d6abf379769711c7c5cd4a4a20c52a0f8dc0
Nandini Goud <nandiniburra22@gmail.com>
2023-03-03 22:26:49 +0530

    first commit

---
+++ app/src/main/java/com/example/groceryapp/GroceryViewModelFactory.kt
@@ -0,0 +1,12 @@
+package com.example.groceryapp
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.grocerylist.Database.GroceryRepository
+
+class GroceryViewModelFactory(private val repository: GroceryRepository):ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return GroceryViewModel(repository) as T
+    }
+}
